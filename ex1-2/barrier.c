b5d94578f196d366e5446dab98bef8bb6ce3c714
ArgVampir <kris_.k@hotmail.com>
2020-10-13 18:11:40 +0800

    fix turnstile post and wait

--- ex1-2/barrier.c
+++ ex1-2/barrier.c
@@ -13,7 +13,7 @@ void barrier_init ( barrier_t *barrier, int count ) {
     barrier->n = 0;
     sem_init(&barrier->mutex, 0, 1);
     sem_init(&barrier->turnstile1, 0, 0);
-    sem_init(&barrier->turnstile2, 0, 0);
+    sem_init(&barrier->turnstile2, 0, 1);
     // sem_init(&barrier->number_, 0, 1);
     // sem_init(&barrier->fizz_, 0, 0);
     // sem_init(&barrier->buzz_, 0, 0);
@@ -23,22 +23,26 @@ void barrier_init ( barrier_t *barrier, int count ) {
 void phase1_barrier(barrier_t *barrier) {
   sem_wait(&barrier->mutex);
   if (++barrier->n == barrier->count) {
+      sem_wait(&barrier->turnstile2);  //0
       sem_post(&barrier->turnstile1);
   }
   sem_post(&barrier->mutex);
+
   sem_wait(&barrier->turnstile1);
-  sem_post(&(barrier->turnstile1));
+  sem_post(&(barrier->turnstile1)); // 1
 }
 
 void phase2_barrier(barrier_t *barrier)
 {
   sem_wait(&barrier->mutex);
   if (--barrier->n == 0) {
+      sem_wait(&barrier->turnstile1);
       sem_post(&barrier->turnstile2);
   }
   sem_post(&barrier->mutex);
+
   sem_wait(&barrier->turnstile2);
-  sem_wait(&barrier->turnstile2);
+  sem_post(&barrier->turnstile2);
 }
 
 void barrier_wait ( barrier_t *barrier ) {
