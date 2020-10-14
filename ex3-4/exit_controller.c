e67c1c9b301e622e6bd5a3b2eee72ebfed944d4a
ArgVampir <kris_.k@hotmail.com>
2020-10-14 23:16:04 +0800

    add exit controller

--- ex3-4/exit_controller.c
+++ ex3-4/exit_controller.c
@@ -5,15 +5,94 @@
  * as the method signatures will be needed to compile with the runner.
  */
 #include "exit_controller.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-void exit_controller_init(exit_controller_t *exit_controller, int no_of_priorities) {
+void exit_controller_init(exit_controller_t *exit_controller, int no_of_priorities)
+{
+    exit_controller->noOfPriorities = no_of_priorities;
+    exit_controller->highestPriority = no_of_priorities - 1;
+    exit_controller->isFirst = 1;
+    exit_controller->hasQueue = 0;
+
+    exit_controller->priorityArrPtr = malloc(sizeof(int) * no_of_priorities);
+    for (int i = 0; i < no_of_priorities; i++)
+    {
+        *(exit_controller->priorityArrPtr + i) = 0;
+    }
+
+    sem_init(&(exit_controller->mutex1), 0, 1);
+    sem_init(&(exit_controller->semaphore), 0, 1);
+
+    exit_controller->semaphoreArrPtr = malloc(sizeof(sem_t) * no_of_priorities);
+    //exit_controller->semaphoreArrPtr = malloc(sizeof(sem_t) * no_of_priorities);
+    for (int i = 0; i < no_of_priorities; i++)
+    {
+        sem_init(exit_controller->semaphoreArrPtr + i, 0, 0);
+    }
 }
 
-void exit_controller_wait(exit_controller_t *exit_controller, int priority) {
+void exit_controller_wait(exit_controller_t *exit_controller, int priority)
+{
+    sem_wait(&(exit_controller->mutex1));
+    exit_controller->isFirst = 1;
+    for (int i = 0; i < exit_controller->noOfPriorities; i++)
+    {
+        if (*(exit_controller->priorityArrPtr + i) != 0)
+        {
+            exit_controller->isFirst = 0;
+        }
+    }
+
+    (*(exit_controller->priorityArrPtr + priority))++;
+
+    if (exit_controller->isFirst != 1)
+    {
+        sem_post(&(exit_controller->mutex1));
+        sem_wait((exit_controller->semaphoreArrPtr + priority));
+    }
+    else
+    {
+        sem_post(&(exit_controller->mutex1));
+    }
+
+    sem_wait(&(exit_controller->semaphore));
 }
 
-void exit_controller_post(exit_controller_t *exit_controller, int priority) {
+void exit_controller_post(exit_controller_t *exit_controller, int priority)
+{
+    sem_wait(&(exit_controller->mutex1));
+    exit_controller->hasQueue = 0;
+
+    exit_controller->highestPriority = exit_controller->noOfPriorities - 1;
+
+    (*(exit_controller->priorityArrPtr + priority))--;
+    for (int i = 0; i < exit_controller->noOfPriorities; i++)
+    {
+        if (*(exit_controller->priorityArrPtr + i) != 0)
+        {
+            exit_controller->hasQueue = 1;
+            exit_controller->highestPriority = i;
+            break;
+        }
+    }
+    if (exit_controller->hasQueue == 1)
+    {
+        // We call for (one of) the thread that has the highest priority
+        sem_post((exit_controller->semaphoreArrPtr + exit_controller->highestPriority));
+    }
+    sem_post(&(exit_controller->semaphore));
+    sem_post(&(exit_controller->mutex1));
 }
 
-void exit_controller_destroy(exit_controller_t *exit_controller){
+void exit_controller_destroy(exit_controller_t *exit_controller)
+{
+    for (int i = 0; i < exit_controller->noOfPriorities; i++)
+    {
+        sem_destroy(exit_controller->semaphoreArrPtr + i);
+    }
+    sem_destroy(&(exit_controller->mutex1));
+    sem_destroy(&(exit_controller->semaphore));
+    free(exit_controller->priorityArrPtr);
+    free(exit_controller->semaphoreArrPtr);
 }
--- ex3-4/exit_controller.h
+++ ex3-4/exit_controller.h
@@ -11,14 +11,18 @@
 
 #define MAX_PRIORITIES 5000 // we set a limit on how many possible priorities we have
 
-typedef struct exit_controller {
-    // define your variables here
+typedef struct exit_controller
+{
+    int noOfPriorities, highestPriority, isFirst, hasQueue;
+    int *priorityArrPtr;
+    sem_t mutex1;
+    sem_t semaphore;
+    sem_t *semaphoreArrPtr;
 } exit_controller_t;
 
 void exit_controller_init(exit_controller_t *exit_controller, int no_of_priorities);
 void exit_controller_wait(exit_controller_t *exit_controller, int priority);
 void exit_controller_post(exit_controller_t *exit_controller, int priority);
 void exit_controller_destroy(exit_controller_t *exit_controller);
-
 
 #endif // __CS2106_EXIT_CONTROLLER_H_
